48f9005405abd699ce740e2dd3f03d866d5ec308
Jacob <jlee93115@gmail.com>
2019-11-09 11:40:29 -0800

    moving drone with velocity and duration

---
+++ vmove.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+© Copyright 2015-2016, 3D Robotics.
+guided_set_speed_yaw.py: (Copter Only)
+
+This example shows how to move/direct Copter and send commands in GUIDED mode using DroneKit Python.
+
+Example documentation: http://python.dronekit.io/examples/guided-set-speed-yaw-demo.html
+"""
+from __future__ import print_function
+
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+from pymavlink import mavutil  # Needed for command message definitions
+import time
+import math
+
+# Set up option parsing to get connection string
+import argparse
+
+parser = argparse.ArgumentParser(description='Control Copter and send commands in GUIDED mode ')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
+
+
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:  # Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+# Arm and take of to altitude of 5 meters
+arm_and_takeoff(5)
+
+"""
+Convenience functions for sending immediate/guided mode commands to control the Copter.
+
+The set of commands demonstrated here include:
+* MAV_CMD_CONDITION_YAW - set direction of the front of the Copter (latitude, longitude)
+* MAV_CMD_DO_SET_ROI - set direction where the camera gimbal is aimed (latitude, longitude, altitude)
+* MAV_CMD_DO_CHANGE_SPEED - set target speed in metres/second.
+
+
+The full set of available commands are listed here:
+http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/
+"""
+
+
+def condition_yaw(heading, relative=True):
+    """
+    Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
+
+    This method sets an absolute heading by default, but you can set the `relative` parameter
+    to `True` to set yaw relative to the current yaw heading.
+
+    By default the yaw of the vehicle will follow the direction of travel. After setting
+    the yaw using this function there is no way to return to the default yaw "follow direction
+    of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
+
+    For more information see:
+    http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
+    """
+    if relative:
+        is_relative = 1  # yaw relative to direction of travel
+    else:
+        is_relative = 0  # yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,  # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW,  # command
+        0,  # confirmation
+        heading,  # param 1, yaw in degrees
+        0,  # param 2, yaw speed deg/s
+        1,  # param 3, direction -1 ccw, 1 cw
+        is_relative,  # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)  # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
+
+def set_roi(location):
+    """
+    Send MAV_CMD_DO_SET_ROI message to point camera gimbal at a
+    specified region of interest (LocationGlobal).
+    The vehicle may also turn to face the ROI.
+
+    For more information see:
+    http://copter.ardupilot.com/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_do_set_roi
+    """
+    # create the MAV_CMD_DO_SET_ROI command
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,  # target system, target component
+        mavutil.mavlink.MAV_CMD_DO_SET_ROI,  # command
+        0,  # confirmation
+        0, 0, 0, 0,  # params 1-4
+        location.lat,
+        location.lon,
+        location.alt
+    )
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
+
+"""
+Functions to make it easy to convert between the different frames-of-reference. In particular these
+make it easy to navigate in terms of "metres from the current position" when using commands that take 
+absolute positions in decimal degrees.
+
+The methods are approximations only, and may be less accurate over longer distances, and when close 
+to the Earth's poles.
+
+Specifically, it provides:
+* get_location_metres - Get LocationGlobal (decimal degrees) at distance (m) North & East of a given LocationGlobal.
+* get_distance_metres - Get the distance between two LocationGlobal objects in metres
+* get_bearing - Get the bearing in degrees to a LocationGlobal
+"""
+
+
+def get_location_metres(original_location, dNorth, dEast):
+    """
+    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
+    specified `original_location`. The returned LocationGlobal has the same `alt` value
+    as `original_location`.
+
+    The function is useful when you want to move the vehicle around specifying locations relative to
+    the current vehicle position.
+
+    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+
+    For more information see:
+    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+    """
+    earth_radius = 6378137.0  # Radius of "spherical" earth
+    # Coordinate offsets in radians
+    dLat = dNorth / earth_radius
+    dLon = dEast / (earth_radius * math.cos(math.pi * original_location.lat / 180))
+
+    # New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180 / math.pi)
+    newlon = original_location.lon + (dLon * 180 / math.pi)
+    if type(original_location) is LocationGlobal:
+        targetlocation = LocationGlobal(newlat, newlon, original_location.alt)
+    elif type(original_location) is LocationGlobalRelative:
+        targetlocation = LocationGlobalRelative(newlat, newlon, original_location.alt)
+    else:
+        raise Exception("Invalid Location object passed")
+
+    return targetlocation;
+
+
+def get_distance_metres(aLocation1, aLocation2):
+    """
+    Returns the ground distance in metres between two LocationGlobal objects.
+
+    This method is an approximation, and will not be accurate over large distances and close to the
+    earth's poles. It comes from the ArduPilot test code:
+    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+    """
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat * dlat) + (dlong * dlong)) * 1.113195e5
+
+
+def get_bearing(aLocation1, aLocation2):
+    """
+    Returns the bearing between the two LocationGlobal objects passed as parameters.
+
+    This method is an approximation, and may not be accurate over large distances and close to the
+    earth's poles. It comes from the ArduPilot test code:
+    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+    """
+    off_x = aLocation2.lon - aLocation1.lon
+    off_y = aLocation2.lat - aLocation1.lat
+    bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
+    if bearing < 0:
+        bearing += 360.00
+    return bearing;
+
+"""
+Functions that move the vehicle by specifying the velocity components in each direction.
+The two functions use different MAVLink commands. The main difference is
+that depending on the frame used, the NED velocity can be relative to the vehicle
+orientation.
+
+The methods include:
+* send_ned_velocity - Sets velocity components using SET_POSITION_TARGET_LOCAL_NED command
+* send_global_velocity - Sets velocity components using SET_POSITION_TARGET_GLOBAL_INT command
+"""
+
+
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+    """
+    Move vehicle in direction based on specified velocity vectors and
+    for the specified duration.
+
+    This uses the SET_POSITION_TARGET_LOCAL_NED command with a type mask enabling only
+    velocity components
+    (http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/#set_position_target_local_ned).
+
+    Note that from AC3.3 the message should be re-sent every second (after about 3 seconds
+    with no message the velocity will drop back to zero). In AC3.2.1 and earlier the specified
+    velocity persists until it is canceled. The code below should work on either version
+    (sending the message multiple times does not cause problems).
+
+    See the above link for information on the type_mask (0=enable, 1=ignore).
+    At time of writing, acceleration and yaw bits are ignored.
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,  # time_boot_ms (not used)
+        0, 0,  # target system, target component
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,  # frame
+        0b0000111111000111,  # type_mask (only speeds enabled)
+        0, 0, 0,  # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z,  # x, y, z velocity in m/s
+        0, 0, 0,  # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)  # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+
+    # send command to vehicle on 1 Hz cycle
+    for x in range(0, duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+
+
+"""
+Fly the vehicle in a line using velocity vectors (the underlying code calls the 
+SET_POSITION_TARGET_LOCAL_NED command with the velocity parameters enabled).
+
+The thread sleeps for a time (DURATION) which defines the distance that will be travelled.
+
+The code also sets the yaw (MAV_CMD_CONDITION_YAW) using the `set_yaw()` method in each segment
+so that the front of the vehicle points in the direction of travel
+"""
+"""
+TGB
+send_ned_velocity will move the drone in a particular direction for DURATION seconds
+
+The direction of movement can be relative to the drone's current heading, or relative to actual compass North, South,
+East, West directions. The frame of reference can be changed within send_ned_velocity:
+    MAV_FRAME_BODY_OFFSET_NED - makes velocity component relative to drone's current heading (Currently used)
+                                ex. North = front of drone, South = back of drone, East = right, West = left
+    MAV_FRAME_LOCAL_NED - velocity component is relative to actual North, South, East, West directions
+                            ex. if component is facing south, but velocity component is x m/s East, drone flies East
+    
+    condition_yaw(x) - makes the drone turn (yaw) by x degrees. Can be set as relative to absolute.
+                        relative - if facing east and asked to turn 20deg, drone turns 2- deg clockwise from current heading
+                        absolute - if facing east and asked to turn 180 degrees, drone will face sout
+
+"""
+
+# set flight duration in seconds
+DURATION = 20
+
+# Set up velocity vector to map to each direction.
+# vx > 0 => fly North
+# vx < 0 => fly South
+NORTH = 2
+SOUTH = -2
+
+# Note for vy:
+# vy > 0 => fly East
+# vy < 0 => fly West
+EAST = 2
+WEST = -2
+
+# Note for vz:
+# vz < 0 => ascend
+# vz > 0 => descend
+UP = -0.5
+DOWN = 0.5
+
+# Path using velocity
+print("Path using SET_POSITION_TARGET_LOCAL_NED and velocity parameters")
+
+print("Yaw 45 relative")
+condition_yaw(45)
+
+print("Velocity South & up")
+send_ned_velocity(SOUTH, 0, UP, DURATION)
+send_ned_velocity(0, 0, 0, 1)
+
+
+print("Set new home location to current location")
+vehicle.home_location = vehicle.location.global_frame
+print("Get new home location")
+# This reloads the home location in DroneKit and GCSs
+cmds = vehicle.commands
+cmds.download()
+cmds.wait_ready()
+print(" Home Location: %s" % vehicle.home_location)
+
+"""
+The example is completing. LAND at current location.
+"""
+
+print("Setting LAND mode...")
+vehicle.mode = VehicleMode("LAND")
+
+# Close vehicle object before exiting script
+print("Close vehicle object")
+vehicle.close()
+
+# Shut down simulator if it was started.
+if sitl is not None:
+    sitl.stop()
+
+print("Completed")
