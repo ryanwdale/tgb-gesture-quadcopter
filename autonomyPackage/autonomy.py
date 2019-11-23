38f0d01b5dee5d06ccc7e13a1b81b93a386d279a
Jacob <50378644+jlee115@users.noreply.github.com>
2019-11-23 14:32:25 -0800

    updating fly function

--- autonomyPackage/autonomy.py
+++ autonomyPackage/autonomy.py
@@ -2,7 +2,30 @@ from threading import Event, Thread
 from multiprocessing import Pipe
 from dronekit import connect
 
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+    """
+    Move vehicle in direction based on specified velocity vectors and
+    for the specified duration.
 
+    MAV_FRAME_BODY_OFFSET_NED - makes velocity component relative to drone's current heading (Currently used)
+                                ex. North = front of drone, South = back of drone, East = right, West = left
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,  # time_boot_ms (not used)
+        0, 0,  # target system, target component
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,  # frame
+        0b0000111111000111,  # type_mask (only speeds enabled)
+        0, 0, 0,  # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z,  # x, y, z velocity in m/s
+        0, 0, 0,  # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)  # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+
+    # send command to vehicle on 1 Hz cycle
+    for x in range(0, duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+        
+        
 class AutonomyController:
     def __init__(self):
         self.commThread = Thread(target=self.receiveCommand) 
@@ -35,7 +58,7 @@ class AutonomyController:
             else
                 command = res
 
-
+    
     def move(self, move_vector):
         pass
 
@@ -56,11 +79,20 @@ class AutonomyController:
         pass
         #calculate location and check if it is safe to move to that location
 
-    def fly(self, primary_pos, secondary_pos, directional_vector):
+        
+    """
+    x > 0 => fly North
+    x < 0 => fly South
+    y > 0 => fly East
+    y < 0 => fly West
+    z < 0 => ascend
+    z > 0 => descend
+    """
+    def fly(self, x,y,z, FLIGHTDURATION=5):
         currentlyFlying = True
-        while not self.land_event.is_set():
-            if not self.check_location(primary_pos, secondary_pos):
-                self.land()
-            self.move(directional_vector)
-        self.land()
+        if self.check_location:
+            send_ned_velocity(x, y, z, FLIGHTDURATION)
+            send_ned_velocity(0, 0, 0, 1)
+        Else:
+            print("Invalid movement\n")
         currentlyFlying = False
